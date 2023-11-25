235028460e79253646e2254c7c9193225416c9a5
KqkaricK <andrew73-42@yandex.ru>
2023-11-25 13:49:51 +0700

    Update UserAddMovie.xaml.cs
    Вывел проверку в отдельную функцию.

--- Cinema/UserAddMovie.xaml.cs
+++ Cinema/UserAddMovie.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using Npgsql;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Cinema
 {
@@ -25,30 +13,34 @@ namespace Cinema
 
         private void AddMovie_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(NameMovieBox.Text) || string.IsNullOrEmpty(ZalBox.Text))
+            if (!string.IsNullOrEmpty(NameMovieBox.Text) && !string.IsNullOrEmpty(ZalBox.Text))
             {
-                if (SaveMovieMessage.MessageQueue != null)
-                    SaveMovieMessage.MessageQueue.Enqueue("Добавьте название фильма и зал");
+                AddMovieToDataBase();
             }
-            else
-            {
-                string newMovieName = NameMovieBox.Text;
-                int newZalForMovie = ZalBox.SelectedIndex;
-                bool[,] newSeats = new bool[7, 15];
-                for (int i = 0; i < 7; i++)
-                    for (int j = 0; j < 15; j++)
-                        newSeats[i, j] = false;
 
-                using var connection = DatabaseManager.OpenConnection();
-                using var command = new NpgsqlCommand("INSERT INTO public.movie (seats, name, zal) VALUES (@seats, @name, @zal)", connection);
-                command.Parameters.AddWithValue("seats", newSeats);
-                command.Parameters.AddWithValue("name", newMovieName);
-                command.Parameters.AddWithValue("zal", newZalForMovie);
-                command.ExecuteNonQuery();
+            if (SaveMovieMessage.MessageQueue != null)
+                SaveMovieMessage.MessageQueue.Enqueue("Добавьте название фильма и зал");
+        }
 
-                if (SaveMovieMessage.MessageQueue != null)
-                    SaveMovieMessage.MessageQueue.Enqueue("Успешное добавление фильма");
-            }
+        private void AddMovieToDataBase()
+        {
+            NpgsqlConnection connection;
+            NpgsqlCommand command;
+            string newMovieName = NameMovieBox.Text;
+            int newZalForMovie = ZalBox.SelectedIndex;
+            bool[,] newSeats = new bool[7, 15];
+            for (int i = 0; i < 7; i++)
+                for (int j = 0; j < 15; j++)
+                    newSeats[i, j] = false;
+            connection = DatabaseManager.OpenConnection();
+            command = new NpgsqlCommand("INSERT INTO public.movie (seats, name, zal) VALUES (@seats, @name, @zal)", connection);
+            command.Parameters.AddWithValue("seats", newSeats);
+            command.Parameters.AddWithValue("name", newMovieName);
+            command.Parameters.AddWithValue("zal", newZalForMovie);
+            command.ExecuteNonQuery();
+
+            if (SaveMovieMessage.MessageQueue != null)
+                SaveMovieMessage.MessageQueue.Enqueue("Успешное добавление фильма");
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
